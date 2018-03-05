bee182236917fa270c6bd140b65326653f9bffb0
ibtejbc <37054762+ibtejbc@users.noreply.github.com>
2018-03-05 09:51:49 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<style type="text/css">
+body,td,th {
+	color: #000;
+}
+body {
+	background-color: #FFF;
+	height: auto;
+	width: auto;
+}
+</style>
+
+<body>
+
+
+<center>
+
+<img src = "ibte.png" alt = "IBTE Logo" width = "200" height = "100"/> 
+ <h2> IBTE JEFRI BOLKIAH CAMPUS RESOURCES ONLINE BOOKING  </h2>
+<h2> LOGIN BUTTON </h2>
+<hr>
+
+
+
+
+<p>
+  <span style="color: #00F"></span>
+  </p>
+<p>&nbsp;</p>
+<p><span style="color: #00F"><img src="admin1.png" width="128" height="128"></span>
+  <input name="Button" type = "button" onClick = "location = 'AdminLogin.php' " value = "Login as Admin" /><br><br>
+  
+  
+  <span style="color: #00F"></span>
+  <img src="staff1.png" width="128" height="128">
+  <input name="Button" type = "button"  onClick = "location = 'Stafflogin.php' " value = "Login as Staff" />
+  <br><br>
+  
+  <img src="student1.png" width="128" height="128">
+  <input type = "button"  onClick = "location = 'studentlogin.php' " value = "Login as Student" />
+  <br><br>
+  
+</p>
+
+
+
+
+</center>
+</body>
+</html>
+
